508cc03a4999ae63114f80002519072827205518
Julius Dejon <juliusdejon@gmail.com>
2025-02-04 18:53:59 -0500

    added book ride button

--- containers/BookRide.tsx
+++ containers/BookRide.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback, useRef, useContext, useEffect } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, Pressable } from 'react-native';
 import BottomSheet from 'components/BottomSheet';
 import SearchLocation from './SearchLocation';
 import { AppContext } from 'contexts/AppContext';
@@ -19,13 +19,21 @@ const BookRide = () => {
       handleSheetChanges(1)
     }
   }, [pickUp, dropOff]);
+
+  const bookRide = () => {
+    if (pickUp && dropOff) {
+      console.log('here')
+
+    }
+  };
+
   return (
     <BottomSheet
       bottomSheetRef={bottomSheetRef}
       index={bottomSheetState}
       onChange={handleSheetChanges}
     >
-      <View className="w-full flex flex-col">
+      <View className="w-full flex flex-col gap-2">
         <Text className="font-primary text-md">Hey Julius</Text>
         <Text className="font-primary-bold text-2xl font-bold">Where are you going?</Text>
         <SearchLocation
@@ -38,9 +46,19 @@ const BookRide = () => {
           placeholder="Enter drop-off"
           onTouchEnd={handleTouchEnd}
         />
-        <Button title='Book Ride'>
+        <Pressable
+          style={({ pressed }) => [
+            {
+              backgroundColor: pressed ? 'black' : 'grey',
+            },
+          ]}
+          className={`${(pickUp && dropOff) ? 'bg-blue-600' : 'bg-gray-300'} w-full mt-4 h-16 flex  rounded-full items-center justify-center`}
+          onPress={bookRide}>
+          <Text className='text-white font-primary text-xl font-primary-semibold'>
+            Book Ride
+          </Text>
+        </Pressable>
 
-        </Button>
       </View>
     </BottomSheet>
   );
