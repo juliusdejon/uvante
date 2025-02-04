2fe0f81962f8c5a4ded69012c225cd9e88b34f99
Julius Dejon <juliusdejon@gmail.com>
2025-02-04 00:32:56 -0500

    Added build config

--- components/map/index.tsx
+++ components/map/index.tsx
@@ -1,10 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Mapbox from '@rnmapbox/maps';
+
+Mapbox.setAccessToken('pk.eyJ1IjoianVsaXVzZGVqb24iLCJhIjoiY202b3YwcHVlMHRyNTJscTBkYW96bDI4MyJ9.DE9eY_MiUPSbRMsuI7b28g');
+
 
 const Map = () => {
   return (
     <View style={{ flex: 0.5, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Map</Text>
+      <Mapbox.MapView className='flex' />
+
     </View>
   )
 }
