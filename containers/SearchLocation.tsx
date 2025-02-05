9a52028b1ed847fc3f5f03cd38102b0ba278559e
Julius Dejon <juliusdejon@gmail.com>
2025-02-05 16:49:17 -0500

    Refactor Added useLocationSearch

--- containers/SearchLocation.tsx
+++ containers/SearchLocation.tsx
@@ -1,60 +1,30 @@
-import axios from 'axios';
-import React, { useEffect, useRef, useState } from 'react';
-import { View, Text, TouchableOpacity, FlatList, ListRenderItem, TextInput } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { View, Text, TouchableOpacity, FlatList, ListRenderItem } from 'react-native';
 import TextField from 'components/TextField';
 import useDebounce from 'hooks/useDebounce';
-import Config from 'config';
-
 import { Place, Coordinate } from 'types/types';
+import { useLocationSearch } from 'hooks/useLocationSearch';
 
 interface SearchLocationProps {
   placeholder: string;
-  setCoordinates: (coordinates: Coordinate) => void,
+  setCoordinates: (coordinates: Coordinate) => void;
   onTouchEnd: () => void;
 }
 
-const SearchLocation: React.FC<SearchLocationProps> = (props) => {
-  const {
-    setCoordinates,
-    placeholder,
-    onTouchEnd
-  } = props;
+const SearchLocation: React.FC<SearchLocationProps> = ({ placeholder, setCoordinates, onTouchEnd }) => {
   const [location, setLocation] = useState<string>('');
-  const [results, setResults] = useState<Place[]>([]);
-  const debouncedLocation = useDebounce(location, 500);
   const [placeSelected, setPlaceSelected] = useState<boolean>(false);
+  const debouncedLocation = useDebounce(location, 500);
   const textFieldRef = useRef<any>(null);
 
-  const fetchResults = async (query: string) => {
-    if (!query || placeSelected) {
-      setResults([]);
-      return null;
-    }
-    try {
-      const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(query)}.json`;
-      const response = await axios.get(url, {
-        params: {
-          access_token: Config.MAP_BOX_ACCESS_TOKEN
-        }
-      });
-      setResults(response.data.features);
-    } catch (error) {
-      console.error('Error fetching addresses:', error);
-    }
-  };
-
-  useEffect(() => {
-    fetchResults(debouncedLocation);
-  }, [debouncedLocation]);
+  const { results, setResults } = useLocationSearch(debouncedLocation, placeSelected);
 
   const onSelectPlace = (item: Place) => {
     setLocation(item.place_name);
     setCoordinates(item.center);
     setResults([]);
     setPlaceSelected(true);
-    if (textFieldRef.current) {
-      textFieldRef.current.blur();
-    }
+    textFieldRef.current?.blur();
   };
 
   const renderPlace: ListRenderItem<Place> = ({ item }) => (
---
+++ hooks/useLocationSearch.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+import Config from "config";
+import { useState, useEffect } from "react";
+import { Place } from "types/types";
+
+const useLocationSearch = (
+  debouncedLocation: string,
+  placeSelected: boolean
+) => {
+  const [results, setResults] = useState<Place[]>([]);
+
+  useEffect(() => {
+    const fetchResults = async () => {
+      if (!debouncedLocation || placeSelected) {
+        setResults([]);
+        return;
+      }
+      try {
+        const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+          debouncedLocation
+        )}.json`;
+        const response = await axios.get(url, {
+          params: { access_token: Config.MAP_BOX_ACCESS_TOKEN },
+        });
+        setResults(response.data.features);
+      } catch (error) {
+        console.error("Error fetching addresses:", error);
+      }
+    };
+
+    fetchResults();
+  }, [debouncedLocation, placeSelected]);
+
+  return { results, setResults };
+};
+
+export { useLocationSearch };
