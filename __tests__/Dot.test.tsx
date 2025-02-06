4849cb5d7ad85298967446e3b84a0ccbba1f9fed
Julius Dejon <juliusdejon@gmail.com>
2025-02-06 00:00:19 -0500

    Configure jest path

--- __tests__/Dot.test.tsx
+++ __tests__/Dot.test.tsx
@@ -1,11 +1,13 @@
+import React from 'react';
 import { render } from '@testing-library/react-native';
+import Dot from 'components/Dot';
 
-import Dot from '../components/Dot';
+describe('Dot Component', () => {
+  it('renders correctly with the expected styles', () => {
+    const { getByTestId } = render(<Dot />);
+    const dotElement = getByTestId('u-dot');
 
-describe('<Dot />', () => {
-  test('Text renders correctly on Dot', () => {
-    const { getByText } = render(<Dot />);
-
-    getByText('Welcome!');
+    expect(dotElement).toBeTruthy();
+    expect(dotElement.props.className).toContain('w-5 h-5 rounded-full bg-blue-500 border-2 border-white');
   });
 });
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  preset: 'react-native',
+  preset: "react-native",
+  moduleNameMapper: {
+    "^components/(.*)$": "<rootDir>/components/$1",
+  },
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@testing-library)/)',
+    "node_modules/(?!(react-native|@react-native|@testing-library)/)",
   ],
 };
