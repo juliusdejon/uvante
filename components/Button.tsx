80880c9379f6b241b43683b6a6d4c02f5aa241b2
Julius Dejon <juliusdejon@gmail.com>
2025-02-06 02:30:45 -0500

    Fixed button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,31 +1,27 @@
-import { Text, Pressable } from 'react-native';
-import React, { Ref } from 'react';
+import { Text, Pressable } from 'react-native'
+import React from 'react'
 
 interface ButtonProps {
-  className?: string;
-  onPress: () => void;
-  children: React.ReactNode;
+  className?: string,
+  onPress: () => void,
+  children: React.ReactNode
 }
-
-const Button = React.forwardRef((props: ButtonProps, ref: Ref<any>) => {
-  const { onPress, className, children } = props;
+const Button = (props: ButtonProps) => {
+  const { onPress, className, children } = props
   return (
     <Pressable
-      ref={ref}
       style={({ pressed }) => [
         {
           backgroundColor: pressed ? 'black' : 'grey',
         },
-        className === 'bg-blue-600' ? { backgroundColor: '#2563EB' } : { backgroundColor: '#D1D5DB' },
       ]}
-      className={`w-full mt-6 h-[48] flex rounded-full items-center justify-center`}
-      onPress={onPress}
-    >
+      className={`bg-blue-600 ${className} w-full mt-6 h-[48] flex  rounded-full items-center justify-center`}
+      onPress={onPress}>
       <Text className='text-white font-primary text-xl font-primary-semibold'>
         {children}
       </Text>
     </Pressable>
-  );
-});
+  )
+}
 
-export default Button;
+export default Button
