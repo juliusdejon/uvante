772c1b07d254cb3ca5a52e9d7f2e31180bfd2638
Julius Dejon <juliusdejon@gmail.com>
2025-02-06 03:08:54 -0500

    Adjust margin on OngoingRide Screen

--- components/view-ride/OngoingRide.tsx
+++ components/view-ride/OngoingRide.tsx
@@ -1,4 +1,4 @@
-import { Text } from 'react-native'
+import { View, Text } from 'react-native'
 import React from 'react'
 import { BottomSheetView } from '@gorhom/bottom-sheet'
 import { Ride } from '@/api/rides/types'
@@ -13,10 +13,12 @@ const OngoingDrive = (props: OngoingDriveProps) => {
   const { ride } = props;
   return (
     <BottomSheetView className='flex px-8 py-4 gap-2 h-[160px]'>
-      <Text className="font-primary-semibold text-xl">
+      <Text className="font-primary-semibold text-xl ">
         Your Ride has Started!
       </Text>
-      <LinearLoader />
+      <View className="my-4">
+        <LinearLoader />
+      </View>
       <RideDetails ride={ride} />
     </BottomSheetView>
   )
